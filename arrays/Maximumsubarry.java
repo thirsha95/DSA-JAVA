58126c7a70fda8ea76595821f6f8f0d00e989d81
thirsha95 <thirsha85@gmail.com>
2026-03-15 00:04:45 +0530

    git add Maximumsubarry.java

---
+++ arrays/Maximumsubarry.java
@@ -0,0 +1,35 @@
+package arrays;
+
+public class Maximumsubarry {
+  
+public static void main(String[]args){
+    int[] arr ={ -2,1,-3,4,-1,2,1,-5,4};
+    int res = maxisum(arr);
+    System.out.println(res);
+
+
+
+}
+
+public static int maxisum(int[] nums){
+  int maximumsum = nums[0];
+  int currentsum = 0;
+
+  for( int num : nums){
+    if(currentsum < 0 ){
+        currentsum = 0;
+    }
+    currentsum += num;
+    
+
+    if( currentsum > maximumsum){
+      maximumsum = currentsum;
+  }
+
+  }
+
+  return maximumsum;
+}
+
+    
+}
