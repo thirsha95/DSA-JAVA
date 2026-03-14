f790ad6848e6dd57346086eba15fc22d36cad917
thirsha95 <thirsha85@gmail.com>
2026-03-15 00:07:51 +0530

    git add Maximumsubarry.java

--- arrays/Maximumsubarry.java
+++ arrays/Maximumsubarry.java
@@ -1,3 +1,15 @@
+/*
+Problem: Maximum Subarray Sum
+Algorithm: Kadane's Algorithm
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+Example:
+Input: [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Subarray: [4,-1,2,1]
+*/
+
 package arrays;
 
 public class Maximumsubarry {
