1d26d5106657ce695bbd12309c99f1aec9a8738d
thirsha95 <thirsha85@gmail.com>
2026-04-09 20:41:22 +0530

    feat: search maximum element in array

---
+++ arrays/MaximumElement.java
@@ -0,0 +1,34 @@
+/*
+Program: Find Maximum Element in an Array
+
+Description:
+This program finds the largest element in an array by traversing all elements.
+It compares each value and keeps track of the maximum found.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+*/
+import java.util.*;
+public class MaximumElement {
+    public static void main (String[] args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[]arr = new int[n];
+     for(int i=0; i<arr.length; i++){
+        arr[i]=sc.nextInt();
+     }
+     int result = maxelement(arr);
+     System.out.println("output " +result);
+
+    }
+    public static int maxelement(int[]arr){
+        int max = arr[0];
+        for(int i=1; i<arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+     return max;
+    }
+}
