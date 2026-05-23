5302bf83e872042d05e2f934416c0f959e3e3272
thirsha95 <thirsha85@gmail.com>
2026-05-23 22:30:45 +0530

    feat: reverse string

---
+++ Strings/BASICS/Reversestring.java
@@ -0,0 +1,14 @@
+package Strings.BASICS;
+import java.util.*;
+public class Reversestring{
+public  static void main(String[] args){
+   Scanner sc = new Scanner(System.in);
+   String str = sc.nextLine();
+   
+   String reversed = " " ;
+   for(int i=str.length()-1; i>=0; i--){
+    reversed = reversed + str.charAt(i);
+   }
+    System.out.print(reversed);
+    }
+}
