363da1c8b4cd9ae146a606d0ca78fe2075e12a22
thirsha95 <thirsha85@gmail.com>
2026-06-12 23:05:46 +0530

    feat:  column-wise matrix print

---
+++ Matrix/Easy/Columnwiseprint.java
@@ -0,0 +1,23 @@
+package Matrix.Easy;
+
+
+import java.util.*;
+public class Columnwiseprint {
+    public static void main(String [] args){
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      int m = sc.nextInt();
+      int[][]matrix = new int[m][n];
+      for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            matrix[i][j]=sc.nextInt();
+        }
+      }
+
+    for(int i=0; i<n; i++ ){
+        for(int j=0; j<m; j++){
+           System.out.print(matrix[i][j] +" ");
+        }
+      }
+    }
+}
