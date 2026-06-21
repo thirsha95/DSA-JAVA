8f8b0fb6c3229a46d5d0a2621bdbedcfe9bf3779
thirsha95 <thirsha85@gmail.com>
2026-06-21 14:55:41 +0530

    feat: add matrix input structure for sum calculation

---
+++ Matrix/Easy/SumOfAllElementsinMatrix.java
@@ -0,0 +1,23 @@
+package Matrix.Easy;
+import java.util.*;
+public class SumOfAllElementsinMatrix {
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int m = sc.nextInt();
+    int[][] matrix = new int [n][m];
+    for(int i=0; i<n ; i++){
+        for(int j=0; j<m; j++){
+            matrix[i][j]=sc.nextInt();
+        }
+    }
+   //logic;
+   int sum=0;
+   for(int i=0 ; i<n; i++){
+    for(int j=0; j<m; j++){
+       sum +=matrix[i][j];
+    }
+   }
+    System.out.println( "sum of matrix " + sum);
+    }
+}
