63acabb16e19e1fd1bda49d9d907f73b318dee16
thirsha95 <thirsha85@gmail.com>
2026-06-21 14:16:10 +0530

    feat: add Maximum Subarray (Kadane's Algorithm)

---
+++ arrays/Medium/Maximumsubarry.java
@@ -0,0 +1,48 @@
+
+/*
+Problem: Maximum Subarray Sum
+Algorithm: Kadane's Algorithm
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+Example:
+Input: [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Subarray: [4,-1,2,1]
+*/
+
+package arrays.Medium;
+
+public class Maximumsubarry {
+  
+public static void main(String[]args){
+    int[] arr ={ -2,1,-3,4,-1,2,1,-5,4};
+    int res = maxisum(arr);
+    System.out.println(res);
+
+
+
+}
+
+public static int maxisum(int[] nums){
+  int maximumsum = nums[0];
+  int currentsum = 0;
+
+  for( int num : nums){
+    if(currentsum < 0 ){
+        currentsum = 0;
+    }
+    currentsum += num;
+
+
+    if( currentsum > maximumsum){
+      maximumsum = currentsum;
+  }
+
+  }
+
+  return maximumsum;
+}
+
+    
+}
