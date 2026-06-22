fa1fd99652b515fa76df09d701d635cc23993d72
thirsha95 <thirsha85@gmail.com>
2026-06-22 22:39:13 +0530

    feat: add Move Zeroes solution

---
+++ arrays/Easy/MoveZeroes.java
@@ -0,0 +1,41 @@
+/*
+Program: MoveZeroes.java
+Problem: Move Zeroes
+Description: Moves all zeros to the end of the array while maintaining the relative order of non-zero elements.
+*/
+package arrays.Easy;
+import java.util.*;
+public class MoveZeroes {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int j=0;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]!=0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
+/*
+   i -> traverses the entire array.
+
+   j -> points to the position where the next
+        non-zero element should be placed.
+
+   Whenever a non-zero is found at i,
+   swap nums[i] with nums[j] and move j forward.
+
+   Result:
+   All non-zero elements move to the front,
+   and all zeros automatically move to the end.
+*/
