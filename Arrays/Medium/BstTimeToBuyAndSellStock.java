7ecde2f7d3e5e1fb43b9cba5327f5c4f999026b4
thirsha95 <thirsha85@gmail.com>
2026-06-22 23:29:13 +0530

    Add BestTimeToBuyAndSellStock

--- Arrays/Medium/BstTimeToBuyAndSellStock.java
+++ Arrays/Medium/BstTimeToBuyAndSellStock.java
@@ -7,7 +7,7 @@ Approach:
 - Calculate max profit greedily
 */
 
-package arrays.Medium;
+package Arrays.Medium;
 class BstTimeToBuyAndSellStock{
 public static void main (String[]args){
     int[] prices = {7,1,5,3,6,4};
