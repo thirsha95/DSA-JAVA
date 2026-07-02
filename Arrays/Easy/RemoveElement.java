efc464095448aa4a11677bcf3b906a9e32b1bcb6
thirsha95 <thirsha85@gmail.com>
2026-07-02 06:14:50 +0530

    feat: add Remove Element solution returning filtered array

--- Arrays/Easy/RemoveElement.java
+++ Arrays/Easy/RemoveElement.java
@@ -1,38 +1,56 @@
-// Problem: Remove Element
-// Topic: Array
-// Pattern: Two Pointers (Filter / Overwrite)
+/*
+Program: Remove Element
+Problem:
+Remove all occurrences of a given value from an array and
+return a new array containing only the remaining elements.
 
-// Idea:
-// Use two pointers:
-// i -> read pointer (traverses array)
-// k -> write pointer (stores valid elements)
-// Copy elements not equal to val to front
+Sample Input:
+nums = {3, 2, 2, 3}
+val = 3
+
+Sample Output:
+Remaining Elements: 2 2
+*/
+
 package Arrays.Easy;
 
 public class RemoveElement {
-    
-    public static void main (String[] args){
-    int[] nums = {3,2,2,3};
-    int val = 3;
-    RemoveElement obj = new RemoveElement();
-    int res = obj.removenum(nums,val);
-    System.out.print("nums : ") ;
-    for(int i=0; i<res; i++){
-    System.out.print(nums[i] + " ");
-    }
-    System.out.println(" ");
-    System.out.println("count : "+res);
 
-    }
+    public static void main(String[] args) {
 
-    public  int removenum(int nums[],int val ){
-        int p = 0 ;
-        for(int i=0; i<nums.length; i++){
-           if(nums[i] != val){
-            nums[p] = nums[i];
-            p++;
-           }
+        int[] nums = {3, 2, 2, 3};
+        int val = 3;
+
+        int[] res = removenum(nums, val);
+
+        System.out.print("Remaining Elements: ");
+        for (int i = 0; i < res.length; i++) {
+            System.out.print(res[i] + " ");
         }
-        return p;
+    }
+
+    // i -> Read pointer
+    // p -> Write pointer
+    public static int[] removenum(int[] nums, int val) {
+
+        int p = 0;
+
+        // Copy valid elements to the front
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[p] = nums[i];
+                p++;
+            }
+        }
+
+        // Create result array of exact size
+        int[] ans = new int[p];
+
+        // Copy valid elements
+        for (int i = 0; i < p; i++) {
+            ans[i] = nums[i];
+        }
+
+        return ans;
     }
 }
